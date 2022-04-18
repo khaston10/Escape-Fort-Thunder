b4f3c9f6d3a814a3ed8dd7d261fea22a53d64c83
Kevin Haston <kevohaston@gmail.com>
2022-04-17 18:24:50 -0700

    Start Menu works.

---
+++ Escape Fort Thunder/Assets/Scipts/GlobalController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GlobalController : MonoBehaviour
+{
+    public static GlobalController Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Escape Fort Thunder/Assets/Scipts/StartContoller.cs
@@ -0,0 +1,221 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartContoller : MonoBehaviour
+{
+    #region Variables - Buttons
+    enum SelectionSate : int
+    {
+        StartGame, CreatFort,
+    }
+
+    SelectionSate currentSelectionState;
+    int selctionIndex = 2;
+
+    public Button[] startGameButtons;
+    public Button[] createFortButtons;
+
+
+    List<Button[]> selectionButtons = new List<Button[]>();
+
+    #endregion
+
+    #region Variables - Panels and Text
+
+    public GameObject newGamePanel;
+    public Text newGameDescriptionText;
+    public Text newGameValueText;
+    int fortSize = 5000;
+    int fortHostility = 5;
+    int lootPercentage = 10;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSelectionState = SelectionSate.StartGame;
+
+        // Initialize the selectionButtons array.
+        selectionButtons.Add(startGameButtons);
+        selectionButtons.Add(createFortButtons);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Functions - Buttons - StartGame
+
+    void PressNewGame()
+    {
+        newGamePanel.SetActive(true);
+        selctionIndex = 3;
+        currentSelectionState = SelectionSate.CreatFort;
+        selectionButtons[((int)currentSelectionState)][selctionIndex].Select();
+        UpdateCreateFortText();
+    }
+
+    void PressLoadGame()
+    {
+        print("Load Game");
+    }
+
+    void PressQuitGame()
+    {
+        print("Quit Game");
+    }
+
+    #endregion
+
+    #region Functions - Buttons - CreateFort
+
+    void PressFortSize()
+    {
+        if (fortSize < 10000) fortSize += 1000;
+        else fortSize = 1000;
+        UpdateCreateFortText();
+    }
+
+    void PressFortHostility()
+    {
+        if (fortHostility < 10) fortHostility += 1;
+        else fortHostility = 1;
+        UpdateCreateFortText();
+    }
+
+    void PressLootPercentage()
+    {
+        if (lootPercentage < 100) lootPercentage += 10;
+        else lootPercentage = 10;
+        UpdateCreateFortText();
+    }
+
+    void PressStart()
+    {
+        print("Start");
+    }
+
+    void UpdateCreateFortText()
+    {
+        if (selctionIndex == 3)
+        {
+            newGameDescriptionText.text = "Fort Size";
+            newGameValueText.text = fortSize.ToString();
+        }
+
+        else if (selctionIndex == 2)
+        {
+            newGameDescriptionText.text = "Fort Hostility";
+            newGameValueText.text = fortHostility.ToString();
+        }
+
+        else if (selctionIndex == 1)
+        {
+            newGameDescriptionText.text = "Loot Percentage";
+            newGameValueText.text = lootPercentage.ToString() + "%";
+        }
+
+        else if (selctionIndex == 0)
+        {
+            newGameDescriptionText.text = "Start";
+            newGameValueText.text = "";
+        }
+
+
+    }
+
+    void BackToStartGameState()
+    {
+        currentSelectionState = SelectionSate.StartGame;
+        selctionIndex = 2;
+        selectionButtons[((int)currentSelectionState)][selctionIndex].Select();
+        newGamePanel.SetActive(false);
+
+    }
+
+    #endregion
+
+    #region Functions - Main Controller
+
+    void IncreaseDecreaseSelectionIndex(bool forward)
+    {
+
+        // If the selection index needs to increase.
+        if (forward)
+        {
+            if (selctionIndex < selectionButtons[((int)currentSelectionState)].Length - 1)
+            {
+                selctionIndex += 1;
+            }
+        }
+
+        // If the selection index needs to decrease.
+        else
+        {
+            if (selctionIndex > 0)
+            {
+                selctionIndex -= 1;
+            }
+        }
+
+    }
+
+    public void PressA()
+    {
+        if (currentSelectionState == SelectionSate.StartGame)
+        {
+            if (selctionIndex == 0) PressQuitGame();
+            else if (selctionIndex == 1) PressLoadGame();
+            else if (selctionIndex == 2) PressNewGame();
+        }
+
+        else if (currentSelectionState == SelectionSate.CreatFort)
+        {
+            if (selctionIndex == 0) PressStart();
+            else if (selctionIndex == 1) PressLootPercentage();
+            else if (selctionIndex == 2) PressFortHostility();
+            else if (selctionIndex == 3) PressFortSize();
+            UpdateCreateFortText();
+            selectionButtons[((int)currentSelectionState)][selctionIndex].Select();
+        }
+    }
+
+    public void PressB()
+    {
+        if (currentSelectionState == SelectionSate.CreatFort)
+        {
+            BackToStartGameState();
+        }
+    }
+
+    public void PressUp()
+    {
+        IncreaseDecreaseSelectionIndex(true);
+        selectionButtons[((int)currentSelectionState)][selctionIndex].Select();
+
+        if (currentSelectionState == SelectionSate.CreatFort)
+        {
+            UpdateCreateFortText();
+        }
+    }
+
+    public void PressDown()
+    {
+        IncreaseDecreaseSelectionIndex(false);
+        selectionButtons[((int)currentSelectionState)][selctionIndex].Select();
+
+        if (currentSelectionState == SelectionSate.CreatFort)
+        {
+            UpdateCreateFortText();
+        }
+    }
+
+    #endregion
+
+   
+}
